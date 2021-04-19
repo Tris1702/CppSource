f641467f6bd30ae8df988eeb690d5d52b023f93d
Tris1702 <phuonghoand2001@gmail.com>
2021-04-19 17:05:08 +0700

    newfile

---
+++ newfile.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+#define mp make_pair
+#define F first
+#define S second
+using namespace std;
+
+typedef long long ll;
+
+void prepare()
+{
+
+}
+
+void init()
+{
+
+}
+
+void solve()
+{
+    
+}
+int main()
+{
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
